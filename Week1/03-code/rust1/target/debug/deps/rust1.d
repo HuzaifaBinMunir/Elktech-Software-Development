d3c773cbde535a5e6672dcf2fdc4f4c5a2e1d1bd
Huzaifa Bin Munir <binmunirhuzaifa@gmail.com>
2026-01-13 18:36:09 +0500

    Hellow World Go

---
+++ Week1/03-code/rust1/target/debug/deps/rust1.d
@@ -0,0 +1,5 @@
+C:\Users\Huzaifa Bin Munir\Desktop\Elktech\Elktech-Software-Development\Week1\03-code\rust1\target\debug\deps\rust1.d: src\main.rs
+
+C:\Users\Huzaifa Bin Munir\Desktop\Elktech\Elktech-Software-Development\Week1\03-code\rust1\target\debug\deps\rust1.exe: src\main.rs
+
+src\main.rs:
